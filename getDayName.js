6190f337d9bdc2c4a02c09c0544323f3e1b9d8bf
tmug94 <44855697+tmug94@users.noreply.github.com>
2019-03-28 19:45:53 -0400

    Update and rename dayName.js to getDayName.js

--- getDayName.js
+++ getDayName.js
@@ -1,5 +1,5 @@
 // MM/DD/YYYY format
-function dayName(dateString) {
+function getDayName(dateString) {
     var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     var date1 = new Date(dateString)
     return weekDays[date1.getUTCDay()];
